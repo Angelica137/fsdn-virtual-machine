ad20c1b2baa623767f8612f8c718925097539779
Angelica <Angelica137@users.noreply.github.com>
2024-05-30 11:57:36 +0100

    complete set up

--- README.md
+++ README.md
@@ -2,4 +2,3 @@ fullstack-nanodegree-vm
 =============
 
 Common code for the Relational Databases and Full Stack Fundamentals courses
-# fsdn-virtual-machine
--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+import sys
+
+DBNAME = "forum"
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+    """Return all posts form the database, most recent first"""
+    db = psycopg2.connect(database=DBNAME, user='vagrant', password='vagrant', host='localhost', port='5432')
+    c = db.cursor()
+    c.execute("select content, time from posts order by time desc")
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+    """Ass a post to the 'database' with the current timestamp"""
+    db = psycopg2.connect(database=DBNAME, user='vagrant', password='vagrant', host='localhost', port='5432')
+    c = db.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    db.commit()
+    db.close()
